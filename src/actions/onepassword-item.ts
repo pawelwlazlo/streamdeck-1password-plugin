f499405d352287ef955605b2e2d571633e06d600
Paweł Wlazło <pawel@wlazlo.dev>
2026-08-26 19:17:07 +0200

    Usunięcie przykładowej akcji licznika

---
+++ src/actions/onepassword-item.ts
@@ -0,0 +1,4 @@
+import { action, SingletonAction } from "@elgato/streamdeck";
+
+@action({ UUID: "io.nerd4rent.streamdeck-1password-plugin.item" })
+export class OnePasswordItem extends SingletonAction {}
